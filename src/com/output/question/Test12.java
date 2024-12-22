909f97555ec0a0aed8cc40401f2585acef1b785a
Amit Sharma <ksharma.amit1994@gmail.com>
2024-12-22 23:52:44 +0530

    code updated.

---
+++ src/com/output/question/Test12.java
@@ -0,0 +1,26 @@
+package com.output.question;
+/*
+class A {
+    public void m1(A a){
+        System.out.println("m1 method in class A");
+    }
+}
+class B extends A {
+    public void m1(A a){
+        System.out.println("m1 method in class B");
+    }
+}
+public class Test12{
+    public static void main(String[] args){
+        A a = new A();  //this will create object for class A.
+        a.m1(a);  //print   m1 method in class A
+        a.m1(new B());  //print m1 method in class B
+
+        B b = new B();
+        b.m1(null);   //print m1 method in class B
+
+        a = b;
+        a.m1(null); //print m1 method in class B
+        a.m1(new A());  //print   m1 method in class A
+    }
+}*/
